8ac0f6ef46b1fcb4b76628287403d346181cb67f
Fernando Hirsch <144632964+ochocss@users.noreply.github.com>
2025-07-09 17:54:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var prevScrollpos = window.pageYOffset;
+var navbarHeight = document.getElementById("navbar").offsetHeight;
+
+const body = document.body;
+const html = document.documentElement;
+
+const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+
+  console.log(height); /* the second condition is to prevent the navbar to blocking the section when redirecting */
+  if (prevScrollpos > currentScrollPos && !(Math.abs(prevScrollpos-currentScrollPos) != 100 && currentScrollPos != height)) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-108px";
+    document.getElementById("navbar").style.top = (navbarHeight * -1).toString() + "px";
   }
+
   prevScrollpos = currentScrollPos;
 }
