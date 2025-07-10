a9b45d9fdab6563d0016660e2e5beb3f4dc61e66
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-07-09 23:44:06 -0300

    Fix navbar

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var prevScrollpos = window.pageYOffset;
 var navbarHeight = document.getElementById("navbar").offsetHeight;
+document.getElementById("banner-navbar-space").style.minHeight = navbarHeight*2;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos <= navbarHeight/2) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = (navbarHeight * -1).toString() + "px";
@@ -15,4 +16,5 @@ window.onscroll = function() {
 
 window.onresize = () => {
   var navbarHeight = document.getElementById("navbar").offsetHeight;
+  document.getElementById("banner-navbar-space").style.minHeight = navbarHeight.toString() + "px";
 }
