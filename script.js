cc84c4291bcde0269b5921099af556306af98bff
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-07-09 23:25:35 -0300

    Fix section redirection

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 var prevScrollpos = window.pageYOffset;
 var navbarHeight = document.getElementById("navbar").offsetHeight;
 
-const body = document.body;
-const html = document.documentElement;
-
-const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
-  console.log(height); /* the second condition is to prevent the navbar to blocking the section when redirecting */
-  if (prevScrollpos > currentScrollPos && !(Math.abs(prevScrollpos-currentScrollPos) != 100 && currentScrollPos != height)) {
+  if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = (navbarHeight * -1).toString() + "px";
